5a7db267504843c519fafc6474f4d95895cb0e1d
Jozef vanický <jozef.vanicky@gmail.com>
2019-02-18 22:56:52 +0100

    Obmedzenie vstupnych argumentov

--- parse.php
+++ parse.php
@@ -5,3 +5,21 @@
  * VUT Login: xvanic09
  * Date: 2019-02-18
  */
+
+
+if ($argc>2) {
+    fwrite(STDERR, "Wrong input!\n");
+    exit(10);
+}
+
+if ($argc==2) {
+    if ($argv[1]=="--help") {
+        echo "--help\n";
+        echo "Skript typu filtr (parse.php v jazyce PHP 7.3) nacte ze standardniho vstupu zdrojovy kod v IPPcode19 (viz sekce 6), zkontroluje lexikalni a syntaktickou spravnost kodu a vypise na standardni vystup XML reprezentaci programu dle specifikace v sekci 3.1.\n";
+        exit(0);
+    }
+    else{
+        fwrite(STDERR, "Wrong input!\n");
+        exit(10);
+    }
+}
